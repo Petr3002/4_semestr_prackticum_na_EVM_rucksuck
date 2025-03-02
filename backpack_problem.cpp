f85c2be505c0c631295d400be705a0c16400f20a
Petr3002 <132373869+Petr3002@users.noreply.github.com>
2025-03-03 00:25:39 +0300

    Add files via upload

---
+++ backpack_problem.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct item {
+    int v;
+    int w;
+    double cost;
+    item(int vv, int ww){
+        v = vv;
+        w = ww;
+        cost = ((double) v) / ((double) w);
+    }
+};
+
+bool cmp(item a, item b) {
+    return a.cost > b.cost;
+}
+
+int main() {
+    int n, vv, ww, W;
+    cin >> n >> W;
+    vector<item> a;
+    for (int i = 0; i < n; i++) {
+        cin >> vv >> ww;
+        item q(vv, ww);
+        a.push_back(q);
+    }
+    sort(a.begin(), a.end(), cmp);
+
+    int v = 0, w = 0, v1, w1;
+    vector<bool> items;
+    for (int i = 0; i < n - 1; i++) {
+        v1 = v + a[i].v;
+        w1 = w + a[i].w;
+        if ((w1 > W) or ((v1 + ((double) (W - w1)) * a[i + 1].cost) < (v + ((double) (W - w)) * a[i + 1].cost))) {
+            items.push_back(false);
+        } else {
+            v = v1;
+            w = w1;
+            items.push_back(true);
+        }
+    }
+    cout << "Total value: " << v << "\nFor this we need those items: \n";
+    int cnt = 1;
+    for (int i = 0; i < n; i++) {
+        if (items[i]) {
+            cout << cnt++ << ") value = " << a[i].v << ", weight = " << a[i].w << endl;
+        }
+    }
+    return 0;
+}
